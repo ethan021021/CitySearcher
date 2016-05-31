6ab906e35171feb64790be472307eb4a90fc8128
Ethan Thomas <ethanthomas@Ethans-MacBook-Pro.local>
2016-05-31 15:57:33 -0700

    Initial

---
+++ CitySearcher/ViewController.swift
@@ -0,0 +1,26 @@
+//
+//  ViewController.swift
+//  CitySearcher
+//
+//  Created by Ethan Thomas on 5/31/16.
+//  Copyright © 2016 Ethan Thomas. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
